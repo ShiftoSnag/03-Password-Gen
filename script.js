0afc51eb532d5f5c335219137f7383680fe12ec5
Ryan Mattson <ryan.mattson32@gmail.com>
2023-10-02 16:41:56 -0600

    ADded pasword length adn lower case letters

--- script.js
+++ script.js
@@ -11,3 +11,15 @@ let writePassword = function() {
   let numberList = "1234567890";
   let specialList = "!”#$%&’()*+,-./:;<=>?@[\]^_`{|}~";
   let orderedPw = ""
+
+  let userLength = prompt("Choose password length");
+  while ((userLength < 8 || userLength > 128) || (isNaN(userLength))) {
+    userLength = prompt("*length between 8 and 128");
+  }
+  console.log("The user chose a length of: " + userLength);
+
+  let lowercase = confirm("include lowercase letters? ('ok' for yes and 'cancel' for no)");
+  console.log("The user wants lowercase: " + lowercase);
+  if (lowercase) {  
+    typeCount++;
+  }
